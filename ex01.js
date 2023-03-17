9b59d2985a7ed1bd1b901463dbdeb60ca996a8c7
LucasNoronha92 <donlucatone@gmail.com>
2023-03-17 01:24:08 -0300

    second commit

--- ex01.js
+++ ex01.js
@@ -1,3 +1,8 @@
+/*Dado o Array de inteiros abaixo, ordene-o de tal forma que os números “1” estejam à esquerda.
+Os itens devem ser modificados no lugar, ou seja, você não ira trocar posições e sim colocar os
+números “1” no inicio do Array.
+[2, 1, 5, 2, 5, 2, 1, 1, 1, 7, 9, 13, 127, 21]*/
+
 const lista = [2, 1, 5, 2, 5, 2, 1, 1, 1, 7, 9, 13, 127, 21]
 let isOne = [];
 let notOne = [];
